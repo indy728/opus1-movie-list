573d0e56cba779abb9477f5ac2a8239cc9f7007f
Kyle Murray <kdevlinmurray@gmail.com>
2019-05-21 15:42:36 -0700

    step one of query component

--- src/components/atoms/PosterImage/PosterImage.js
+++ src/components/atoms/PosterImage/PosterImage.js
@@ -1,57 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 
 import PosterImageWrapper from '../PosterImageWrapper';
 
-const query = gql`
-  query GetPosters($title: String)
-  {
-    search(searchTerm: $title) {
-      movies{
-        title
-        year
-        poster
-      }
-    }
-  }
-`;
-
-const postersQueryOptions = {
-  options: ({title}) => ({ variables: { title } }),
-}
-
 const ColumnWrapper = styled.div``;
 
-class PosterImage extends Component {
-    renderPoster() {
-        console.log(this.props.data);
+const PosterImage = ( { movies } ) => {
+  let renderPoster = (movies) => {
+      console.log(movies);
 
-        if (!this.props.data.search.movies) return ;
+      if (!movies) return ;
 
-        const movies = (this.props.data.search.movies.sort((a,b) => {
-          return parseInt(b.year.substring(0, 4) - a.year.substring(0, 4) )
-        }));
-        const movie = (this.props.data.search.movies[0]);
+      const movieArray = (movies.sort((a,b) => {
+        return parseInt(b.year.substring(0, 4) - a.year.substring(0, 4) )
+      }));
+      const movie = (movieArray[0]);
 
-        return (
-            <PosterImageWrapper 
-                src={movie.poster}
-                alt={movie.title}    
-            />
-        )
-    }
-    
-    render () {
-        if (this.props.data.loading) { return <div>Loading...</div>}
-        if (this.props.data.error) {return <div>Error</div>}
-        return (
-            <ColumnWrapper>
-               {this.renderPoster()}   
-            </ColumnWrapper>
-        )
-    }
+      return (
+          <PosterImageWrapper 
+              src={movie.poster}
+              alt={movie.title}    
+          />
+      )
+  }
+
+  return (
+      <ColumnWrapper>
+          {renderPoster(movies)}
+      </ColumnWrapper>
+  )
 }
-
-export default graphql(query, postersQueryOptions)(PosterImage);
+export default PosterImage;
--- src/components/molecules/Poster/Poster.js
+++ src/components/molecules/Poster/Poster.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Query } from 'react-apollo';
 import styled from 'styled-components';
+// import GetPosters from '../../queries/GetPosters'
+import gql from 'graphql-tag';
 
 import PosterDetails from '../PosterDetails';
 import PosterImage from '../../atoms/PosterImage';
+
+const query = gql`
+  query GetPosters($title: String)
+  {
+    search(searchTerm: $title) {
+      movies{
+        title
+        year
+        poster
+      }
+    }
+  }
+`;
+
 
 const Wrapper = styled.div`
     background-color: #f9f9f9;
@@ -11,10 +28,20 @@ const Wrapper = styled.div`
     box-shadow: 0 0 10px -2px #999999;
 `;
 
-const Poster = (props) => {
+const Poster = ( { title, ...props }) => {
     return (
         <Wrapper>
-            <PosterImage title={props.title} />
+            <Query query={query} variables={{ title }}>
+                {({ loading, error, data }) => {
+                    if (loading) return null;
+                    if (error) return `Error! ${error}`;
+                    // TODO: figure out contingency for title error
+                    if (!data.search) return 'No data';
+                    return (
+                        <PosterImage movies={data.search.movies} />
+                    );
+                }}
+            </Query>
             <PosterDetails />
         </Wrapper>
     )
